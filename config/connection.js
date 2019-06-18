0b243777ff89d2bdfd6530bf02e6d7d253521d85
Dan Longo <dan_longo@Dans-MacBook-Pro.local>
2019-06-17 20:44:37 -0500

    monday night

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+if ("u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com") {
+    connection = mysql.createConnection("mysql://t21owgo8sl379esi:ln5iprq3p47q8paz@u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/im3dfxyl5ovvkz5l");
     } else {
         connection = mysql.createConnection({
             host: 'localhost',
