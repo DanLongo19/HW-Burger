7cd734eda328d98f20f12a83275c6075141e6c9b
Dan Longo <dan_longo@Dans-MacBook-Pro.local>
2019-06-18 17:05:53 -0500

    wednesday updated

--- config/controllers/orm.js
+++ config/controllers/orm.js
@@ -2,19 +2,33 @@ var connection = require("../controllers/connection.js");
 
 var orm = {
   all: function(tableInput, cb) {
-    connection.query('SELECT * FROM ', "+tableInput+", function(err, result) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
       console.log(result);
     });
   },
   update: function(tableInput, condition, cb) {
-    connection.query('UPDATE "+tableInput+", SET devoured=true WHERE id='+condition+';' , function(err, result) {
+    var queryString = "UPDATE " + table;
+    queryString += "SET";
+    queryString += deveoured=true;
+    queryString += condition;
+    console.log(queryString);
+    connection.query(queryString , function(err, result) {
       if (err) throw err;
       console.log(result);
     });
   },
   add: function(tableInput, value, cb) {
-    connection.query('INSERT INTO "+tableInput+" (burger_name) VALUES ("+value+");', function(err, result) {
+    var queryString = "INSERT INTO" + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+    console.log(queryString);
+    connection.query(queryString, vals, function(err, result) {
       if (err) throw err;
       console.log(result);
     });
@@ -22,3 +36,5 @@ var orm = {
 };
 
 module.exports = orm;
+
+
