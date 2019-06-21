6bbf06f09119c36e09144d02f560032121b4e56d
Dan Longo <dan_longo@Dans-MBP.attlocal.net>
2019-06-21 10:19:59 -0500

    friday-update

--- models/burger.js
+++ models/burger.js
@@ -4,23 +4,23 @@ var router = express.Router();
 
 var burger = {
     all: function(cb) {
-    orm.all("burgers", function(res){
+      orm.all("burgers", function(res) {
         cb(res);
-    });
+      });
     },
-    update: function(objColVals, condition, cb){
-        orm.update('burgers', objColVals, condition, cb);
-},
-    add: function(cols, vals, cb){
-        orm.add('burgers', cols, vals, function(res){
-            cb(res);
-        });
-}
-}
-router.post('/burgers/create', function(req,res){
-    burger.create(req.body.burger_name, function(result){
-        res.redirect('/');
-    })
-})
-
-module.exports = burger; 
+    create: function(name, cb) {
+      orm.create("burgers", [
+        "burger_name", "devoured"
+      ], [
+        name, false
+      ], cb);
+    },
+    update: function(id, cb) {
+      var condition = "id=" + id;
+      orm.update("burgers", {
+        devoured: true
+      }, condition, cb);
+    }
+  };
+  
+  module.exports = burger;
